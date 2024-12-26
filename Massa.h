3ee7a07b56fb0f9f9ad6508f9f88a4b8dde060ad
Dreh3 <andressa_sousaf@outlook.com>
2024-12-26 19:45:17 -0300

    Código editado para atualização de dados

--- Massa.h
+++ Massa.h
@@ -1,79 +1,94 @@
-//Código desenvolvido por Andressa Sousa Fonseca
+//Código desenvolvido por Andressa Sousa Fonseca 
+ 
 
 #include <stdio.h>
-#include <locale.h>
 #include <stdlib.h>
 
-void converterMassa(){
-	
-	int op=1; //op armazena a opção escolhida pelo usuário
-	float valor, calculo; //valor armazena o valor desejado pelo usuário para conversão e calculo armazenada o resultado da operação de conversão
-	
-	//Permitir acentuação
-	setlocale(LC_ALL, "pt_BR.UTF-8");
+#define K 1000 //mil
+#define M (1000*K) // 1 milhão
+#define B (1000*M) //1 bilhão
 
-		//Loop para permitir que o usuário escolha mais de um opção até que deseje sair
-	while(op!=0){
-	
-    	printf("\n\t Conversão de Massa\n");
-    	printf("1. Converter de toneladas para quilogramas\n2. Converter de quilogramas para toneladas\n3. Converter de quilogramas para gramas\n4. Converter de gramas para quilogramas\n5. Converter de gramas para miligramas\n6. Converter de miligramas para gramas\n0 - Sair\n");
-    	printf("Digite o número da opção desejada (1-6): ");
-    	scanf("%d", &op);
-    	
-    	
-    	switch(op){
-    	    case 1:
-    			//Conversão de toneladas para quilogramas
-    			printf("\n\tDigite o valor em toneladas: ");
-    			scanf("%f", &valor);
-    			calculo=valor*1000;
-    			printf("\n\t%.2f Tonelada(s) = %.2f Quilograma(s)\n",valor, calculo);
-    			break;
-    		case 2:
-    			//Conversão de quilogramas para toneladas
-    			printf("\n\tDigite o valor em quilogramas: ");
-    			scanf("%f", &valor);
-    			calculo=valor/1000.0;
-    			printf("\n\t%.2f Quilograma(s) = %.2f Tonelada(s)\n",valor, calculo);
-    			break;
-    		case 3: 
-    			//Conversão de quilogramas para gramas
-    			printf("\n\tDigite o valor em quilogramas: ");
-    			scanf("%f", &valor);
-    			calculo=valor*1000.0;
-    			printf("\n\t%.2f Quilograma(s) = %.2f Grama(s)\n",valor, calculo);
-    			break;
-    		case 4:
-    			//Conversão de gramas para quilogramas
-    			printf("\n\tDigite o valor em gramas: ");
-    			scanf("%f", &valor);
-    			calculo=valor/1000.0;
-    			printf("\n\t%.2f Grama(s) = %.2f Quilograma(s)\n",valor, calculo);
-    			break;
-    		case 5:
-    		    //Conversão de gramas para miligramas
-    			printf("\n\tDigite o valor em gramas: ");
-    			scanf("%f", &valor);
-    			calculo=valor*1000.0;
-    			printf("\n\t%.2f Grama(s) = %.2f Miligrama(s)\n",valor, calculo);
-    			break;
-    		case 6:
-    		    //Conversão de miligramas para gramas 
-    			printf("\n\tDigite o valor miligramas: ");
-    			scanf("%f", &valor);
-    			calculo=valor/1000.0;
-    			printf("\n\t%.2f Miligrama(s) = %.2f Grama(s)\n",valor, calculo);
-    		    break;    
-    		case 0:
-    			break;
-    		default:
-    			printf("\n\tOpção inexistente\n");
-    			break;
-    	}
-    	if(op!=0){
-        	printf("\nDeseja continuar em conversão de massa? 1. SIM 0. NÃO\t");
-        	scanf("%d", &op);
-    	}
-    	system("clear");
-	}
+float* conversor(int option, float value) {
+    static float converted_values[4] = {0,0,0,0};
+    switch (option)
+    {
+        case 1: //Miligramas
+            converted_values[0] = value; //em miligramas
+            converted_values[1] = value/K; //em gramas
+            converted_values[2] = value/M; //em quilogramas
+            converted_values[3] = value/B; //em toneladas
+            break;
+        case 2://Gramas
+            converted_values[0] = value*K;
+            converted_values[1] = value;
+            converted_values[2] = value/K;
+            converted_values[3] = value/M;
+            break;
+        case 3://Quilogramas
+            converted_values[0] = value*M;
+            converted_values[1] = value*K;
+            converted_values[2] = value;
+            converted_values[3] = value/K;
+            break;
+        case 4://Toneladas
+            converted_values[0] = value*B;
+            converted_values[1] = value*M;
+            converted_values[2] = value*K;
+            converted_values[3] = value;
+            break;
+    }
+
+    return converted_values;
+}
+
+void converterMassa() {
+    while (1) {
+        int option = 0;
+        float value = 0.0;
+        printf("===== Conversor de Massa =====\n\n");
+        printf("Escolha uma das opcoes:\n");
+        printf("\t01 - Miligramas (mg)\n"
+            "\t02 - Gramas (g)\n"
+            "\t03 - Quilogramas (kg)\n"
+            "\t04 - Toneladas (t) \n"
+            "\t0  - Sair\n"
+        "\n\n");
+        printf("> Digite sua opcao: ");
+        scanf("%i",&option);
+
+     if(option == 1 || option == 2 || option == 3 || option == 4) {
+            printf("\n> Digite o valor: ");
+            scanf("%f",&value);
+
+            float* converted_values = conversor(option,value);
+
+            printf("\n+%.*s+%.*s+%.*s+%.*s+\n",
+                25, "-------------------------", 25, "-------------------------",
+                25, "-------------------------", 25, "-------------------------");
+            printf("| %-23s | %-23s | %-23s | %-23s |\n", "Miligramas (mg)", "Gramas (g)", "Quilogramas (kg)", "Toneladas (t)");
+
+            printf("+%.*s+%.*s+%.*s+%.*s+\n",
+                25, "-------------------------", 25, "-------------------------",
+                25, "-------------------------", 25, "-------------------------");
+
+            printf("| %-23.4f | %-23.4f | %-23.4f | %-23.4f |\n",
+                converted_values[0], converted_values[1], converted_values[2], converted_values[3]);
+
+            printf("+%.*s+%.*s+%.*s+%.*s+\n\n",
+                25, "-------------------------", 25, "-------------------------",
+                25, "-------------------------", 25, "-------------------------");
+        }
+        else if(option == 0) break;
+        else printf("\n\nOpcao invalida. ");
+        
+        printf("Aperte ENTER para escolher uma nova opcao...");
+        getchar(); 
+        getchar();
+        
+        #ifdef _WIN32
+            system("cls");
+        #else
+            system("clear");
+        #endif
+    }
 }
