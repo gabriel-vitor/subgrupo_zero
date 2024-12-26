47b91d0bf6156c62f41f9047cacfde676c3a3a8d
Ana Karolina Disigant <anadisigant@gmail.com>
2024-12-26 20:30:02 -0300

    Padronização de velocidade.h

--- velocidade.h
+++ velocidade.h
@@ -1,63 +1,86 @@
 #ifndef VELOCIDADE_H  
 #define VELOCIDADE_H  
 
-#include <stdio.h>  
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MS_KMH 3.6
+#define MPH_KMH 1.609
+#define MS_MPH 2.237
+
+float* conversorVelocidade(int option, float value) {
+    static float converted_values[3] = {0, 0, 0};
+    switch (option) {
+        case 1:
+            converted_values[0] = value;
+            converted_values[1] = value / MS_KMH;
+            break;
+        case 2:
+            converted_values[0] = value;
+            converted_values[1] = value * MS_KMH;
+            break;
+        case 3:
+            converted_values[0] = value;
+            converted_values[1] = value / MPH_KMH;
+            break;
+        case 4:
+            converted_values[0] = value;
+            converted_values[1] = value * MPH_KMH;
+            break;
+        case 5:
+            converted_values[0] = value;
+            converted_values[1] = value * MS_MPH;
+            break;
+        case 6:
+            converted_values[0] = value;
+            converted_values[1] = value / MS_MPH;
+            break;
+    }
+
+    return converted_values;
+}
 
 void converterVelocidade() {
-    int opcao;       
-    float velocidade;
-    float resultado; 
+    while (1) {
+        int opcao = 0;
+        float velocidade = 0.0;
+        printf("\n--- Conversão de Velocidades ---\n"); 
+        printf("1. km/h para m/s\n");                   
+        printf("2. m/s para km/h\n");                   
+        printf("3. km/h para mph\n");                   
+        printf("4. mph para km/h\n");                   
+        printf("5. m/s para mph\n");                    
+        printf("6. mph para m/s\n");                    
+        printf("0. Sair\n");                    
+        printf("Digite sua escolha (1-6): ");           
+        scanf("%d", &opcao);                           
+        
+        if(opcao >= 1 && opcao <= 6) {
+            printf("Digite a velocidade: ");
+            scanf("%f", &velocidade);
+            float* converted_values = conversorVelocidade(opcao, velocidade);
 
-    printf("\n--- Conversão de Velocidades ---\n"); 
-    printf("1. km/h para m/s\n");                   
-    printf("2. m/s para km/h\n");                   
-    printf("3. km/h para mph\n");                   
-    printf("4. mph para km/h\n");                   
-    printf("5. m/s para mph\n");                    
-    printf("6. mph para m/s\n");                    
-    printf("Digite sua escolha (1-6): ");           
-    scanf("%d", &opcao);                           
-    
-    switch (opcao) {
-        case 1: // km/h para m/s
-            printf("Digite a velocidade em km/h: ");
-            scanf("%f", &velocidade);
-            resultado = velocidade / 3.6;
-            printf("%.2f km/h = %.2f m/s\n", velocidade, resultado);
-            break;
-        case 2: // m/s para km/h
-            printf("Digite a velocidade em m/s: ");
-            scanf("%f", &velocidade);
-            resultado = velocidade * 3.6;
-            printf("%.2f m/s = %.2f km/h\n", velocidade, resultado);
-            break;
-        case 3: // km/h para mph
-            printf("Digite a velocidade em km/h: ");
-            scanf("%f", &velocidade);
-            resultado = velocidade / 1.609;
-            printf("%.2f km/h = %.2f mph\n", velocidade, resultado);
-            break;
-        case 4: // mph para km/h
-            printf("Digite a velocidade em mph: ");
-            scanf("%f", &velocidade);
-            resultado = velocidade * 1.609;
-            printf("%.2f mph = %.2f km/h\n", velocidade, resultado);
-            break;
-        case 5: // m/s para mph
-            printf("Digite a velocidade em m/s: ");
-            scanf("%f", &velocidade);
-            resultado = velocidade * 2.237;
-            printf("%.2f m/s = %.2f mph\n", velocidade, resultado);
-            break;
-        case 6: // mph para m/s
-            printf("Digite a velocidade em mph: ");
-            scanf("%f", &velocidade);
-            resultado = velocidade / 2.237;
-            printf("%.2f mph = %.2f m/s\n", velocidade, resultado);
-            break;
-        default: // Caso inválido
-            printf("Opção inválida!\n");
-            break;
+            switch(opcao) {
+                case 1: printf("\n>> %.2f km/h = %.2f m/s\n\n", velocidade, converted_values[1]); break;
+                case 2: printf("\n>> %.2f m/s = %.2f km/h\n\n", velocidade, converted_values[1]); break;
+                case 3: printf("\n>> %.2f km/h = %.2f mph\n\n", velocidade, converted_values[1]); break;
+                case 4: printf("\n>> %.2f mph = %.2f km/h\n\n", velocidade, converted_values[1]); break;
+                case 5: printf("\n>> %.2f m/s = %.2f mph\n\n", velocidade, converted_values[1]); break;
+                case 6: printf("\n>> %.2f mph = %.2f m/s\n\n", velocidade, converted_values[1]); break;
+            }
+        }
+        else if(opcao == 0) break;
+        else printf("Opção inválida!\n");
+
+        printf("Pressione ENTER para escolher uma nova opção...");
+        getchar();
+        getchar();
+
+        #ifdef _WIN32
+            system("cls");
+        #else
+            system("clear");
+        #endif
     }
 }
 #endif
