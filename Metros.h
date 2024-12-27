0a013aca73417f9469749e32598cba4bb99339d6
Lz Spootfy <musquitobrs@gmail.com>
2024-12-26 21:16:17 -0300

    Refatoração Metros.h

--- Metros.h
+++ Metros.h
@@ -1,40 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void converterVolume(float valor, int opcao) {
+#define LITRO_ML 1000
+#define LITRO_MCUBO 0.001
+#define ML_LITRO 0.001
+#define ML_MCUBO 0.000001
+#define MCUBO_LITRO 1000
+#define MCUBO_ML 1000000
+
+float* converterVolume(int opcao, float valor) {
+    static float valores_convertidos[3] = {0, 0, 0};
     switch (opcao) {
         case 1: // Litros para Mililitros e Metros Cúbicos
-            printf("%.2f litros equivalem a %.2f mililitros.\n", valor, valor * 1000);
-            printf("%.2f litros equivalem a %.6f metros cúbicos.\n", valor, valor / 1000);
+            valores_convertidos[0] = valor;
+            valores_convertidos[1] = valor * LITRO_ML;
+            valores_convertidos[2] = valor * LITRO_MCUBO;
             break;
         case 2: // Mililitros para Litros e Metros Cúbicos
-            printf("%.2f mililitros equivalem a %.2f litros.\n", valor, valor / 1000);
-            printf("%.2f mililitros equivalem a %.6f metros cúbicos.\n", valor, valor / 1000000);
+            valores_convertidos[0] = valor;
+            valores_convertidos[1] = valor * ML_LITRO;
+            valores_convertidos[2] = valor * ML_MCUBO;
             break;
         case 3: // Metros cúbicos para Litros e Mililitros
-            printf("%.6f metros cúbicos equivalem a %.2f litros.\n", valor, valor * 1000);
-            printf("%.6f metros cúbicos equivalem a %.2f mililitros.\n", valor, valor * 1000000);
+            valores_convertidos[0] = valor;
+            valores_convertidos[1] = valor * MCUBO_LITRO;
+            valores_convertidos[2] = valor * MCUBO_ML;
             break;
         default:
-            printf("Opção inválida.\n");
+            printf("Opção inválida!\n");
+    }
+    return valores_convertidos;
+}
+
+void conversorVolume() {
+    while (1) {
+        int opcao;
+        float valor;
+        printf("=== Conversor de Unidades de Volume ===\n");
+        printf("1 - Litros para Mililitros e Metros Cubicos\n");
+        printf("2 - Mililitros para Litros e Metros Cubicos\n");
+        printf("3 - Metros cubicos para Litros e Mililitros\n");
+        printf("0 - Sair\n");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
+        
+        if (opcao == 0) break;
+        
+        printf("Digite o valor a ser convertido: ");
+        scanf("%f", &valor);
+        
+        float* resultados = converterVolume(opcao, valor);
+        switch (opcao) {
+            case 1:
+                printf("%.2f litros = %.2f mililitros, %.6f metros cubicos\n", resultados[0], resultados[1], resultados[2]);
+                break;
+            case 2:
+                printf("%.2f mililitros = %.2f litros, %.6f metros cubicos\n", resultados[0], resultados[1], resultados[2]);
+                break;
+            case 3:
+                printf("%.6f metros cubicos = %.2f litros, %.2f mililitros\n", resultados[0], resultados[1], resultados[2]);
+                break;
+        }
+        printf("\nPressione ENTER para continuar...");
+        getchar();
+        getchar();
+        system("clear");
     }
 }
 
-void converterMetros() {
-    float valor;
-    int opcao;
-
-    printf("=== Conversor de Unidades de Volume ===\n");
-    printf("Escolha a conversão desejada:\n");
-    printf("1 - Litros para Mililitros e Metros Cúbicos\n");
-    printf("2 - Mililitros para Litros e Metros Cúbicos\n");
-    printf("3 - Metros Cúbicos para Litros e Mililitros\n");
-    printf("Opção: ");
-    scanf("%d", &opcao);
-
-    printf("Digite o valor a ser convertido: ");
-    scanf("%f", &valor);
-
-    converterVolume(valor, opcao);
-
+int main() {
+    conversorVolume();
     return 0;
 }
